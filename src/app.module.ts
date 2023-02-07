c6389b6e85e088af5d953edd6b249bc66e27fc41
adhamaly <adhamaly126@gmail.com>
2023-02-07 18:07:27 +0200

    Implement PrismaModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { FirebaseModule } from './firebase/firebase.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule, UserModule, FirebaseModule],
+  imports: [AuthModule, UserModule, FirebaseModule, PrismaModule],
 })
 export class AppModule {}
