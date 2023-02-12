8c8319485339b3a19fd515faa79f3c8ea66530ec
adhamaly <adhamaly126@gmail.com>
2023-02-12 12:04:06 +0200

    Enhance server bootstrap

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,11 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { bufferLogs: true });
   app.enableCors({ origin: '*' });
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api/');
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
