a3ab1c79b64d3a2fd269d487dd8be1294dade7dd
adhamaly <adhamaly126@gmail.com>
2023-03-06 01:00:30 +0200

    Add new migrations and enhance categorires seeding

--- prisma/seeds/seed.ts
+++ prisma/seeds/seed.ts
@@ -11,12 +11,160 @@ async function main() {
           {
             nameAr: 'أجهزة الكمبيوتر والأجهزة اللوحية',
             nameEn: 'Computers & tablets',
+            customFields: {
+              create: [
+                {
+                  key: 'screenSize',
+                  type: 'array',
+                  labelAr: 'حجم الشاشة',
+                  labelEn: 'Screen Size',
+                },
+                {
+                  key: 'operatingSystem',
+                  type: 'text',
+                  labelAr: 'نظام التشغيل',
+                  labelEn: 'Operatin System',
+                },
+                {
+                  key: 'releaseYear',
+                  type: 'text',
+                  labelAr: 'سنة الاصدار',
+                  labelEn: 'Release Year',
+                },
+                {
+                  key: 'color',
+                  type: 'array',
+                  labelAr: 'اللون',
+                  labelEn: 'Color',
+                },
+                {
+                  key: 'regionOfManufacture',
+                  type: 'text',
+                  labelAr: 'منظقة المنشأ',
+                  labelEn: 'Region Of Manufacture',
+                },
+                {
+                  key: 'ramSize',
+                  type: 'number',
+                  labelAr: 'حجم الرامات',
+                  labelEn: 'Region Of Manufacture',
+                },
+                {
+                  key: 'processor',
+                  type: 'text',
+                  labelAr: 'بروسيسور',
+                  labelEn: 'Processor',
+                },
+              ],
+            },
           },
-          { nameAr: 'الكاميرات والصور', nameEn: 'Cameras & photos' },
-          { nameAr: 'التلفزيونات والصوتيات', nameEn: 'TVs & Audios' },
+          {
+            nameAr: 'الكاميرات والصور',
+            nameEn: 'Cameras & photos',
+            customFields: {
+              create: [
+                {
+                  key: 'releaseYear',
+                  type: 'text',
+                  labelAr: 'سنة الاصدار',
+                  labelEn: 'Release Year',
+                },
+                {
+                  key: 'color',
+                  type: 'array',
+                  labelAr: 'اللون',
+                  labelEn: 'Color',
+                },
+                {
+                  key: 'regionOfManufacture',
+                  type: 'text',
+                  labelAr: 'منظقة المنشأ',
+                  labelEn: 'Region Of Manufacture',
+                },
+                {
+                  key: 'cameraType',
+                  type: 'array',
+                  labelAr: 'نوع الكاميرا',
+                  labelEn: 'Camera Type',
+                },
+              ],
+            },
+          },
+          {
+            nameAr: 'التلفزيونات والصوتيات',
+            nameEn: 'TVs & Audios',
+            customFields: {
+              create: [
+                {
+                  key: 'screenSize',
+                  type: 'array',
+                  labelAr: 'حجم الشاشة',
+                  labelEn: 'Screen Size',
+                },
+                {
+                  key: 'releaseYear',
+                  type: 'text',
+                  labelAr: 'سنة الاصدار',
+                  labelEn: 'Release Year',
+                },
+                {
+                  key: 'color',
+                  type: 'array',
+                  labelAr: 'اللون',
+                  labelEn: 'Color',
+                },
+                {
+                  key: 'regionOfManufacture',
+                  type: 'text',
+                  labelAr: 'منظقة المنشأ',
+                  labelEn: 'Region Of Manufacture',
+                },
+              ],
+            },
+          },
           {
             nameAr: 'الهواتف المحمولة وملحقاتها',
             nameEn: 'Cell phones & Accessors',
+            customFields: {
+              create: [
+                {
+                  key: 'screenSize',
+                  type: 'array',
+                  labelAr: 'حجم الشاشة',
+                  labelEn: 'Screen Size',
+                },
+                {
+                  key: 'operatingSystem',
+                  type: 'text',
+                  labelAr: 'نظام التشغيل',
+                  labelEn: 'Operatin System',
+                },
+                {
+                  key: 'releaseYear',
+                  type: 'text',
+                  labelAr: 'سنة الاصدار',
+                  labelEn: 'Release Year',
+                },
+                {
+                  key: 'color',
+                  type: 'array',
+                  labelAr: 'اللون',
+                  labelEn: 'Color',
+                },
+                {
+                  key: 'regionOfManufacture',
+                  type: 'text',
+                  labelAr: 'منظقة المنشأ',
+                  labelEn: 'Region Of Manufacture',
+                },
+                {
+                  key: 'material',
+                  type: 'array',
+                  labelAr: 'نوع المادة',
+                  labelEn: 'Material',
+                },
+              ],
+            },
           },
         ],
       },
@@ -50,12 +198,243 @@ async function main() {
           {
             nameAr: 'منزل',
             nameEn: 'House',
+            customFields: {
+              create: [
+                {
+                  key: 'countryId',
+                  type: 'array',
+                  labelAr: 'الدولة',
+                  labelEn: 'Country',
+                },
+                {
+                  key: 'cityId',
+                  type: 'array',
+                  labelAr: 'المدينة',
+                  labelEn: 'City',
+                },
+                {
+                  key: 'age',
+                  type: 'number',
+                  labelAr: 'العمر',
+                  labelEn: 'Age',
+                },
+                {
+                  key: 'numberOfRooms',
+                  type: 'number',
+                  labelAr: 'عدد الغرف',
+                  labelEn: 'Number Of Rooms',
+                },
+                {
+                  key: 'totalArea',
+                  type: 'number',
+                  labelAr: 'المساحة الكلية',
+                  labelEn: 'Total Area',
+                },
+                {
+                  key: 'numberOfFloors',
+                  type: 'number',
+                  labelAr: 'عدد الطوابق',
+                  labelEn: 'Number Of Floors',
+                },
+              ],
+            },
           },
-          { nameAr: 'تاون هاوس', nameEn: 'Townhouse' },
-          { nameAr: 'وحدة', nameEn: 'Unit' },
-          { nameAr: 'فيلا', nameEn: 'Villa' },
-          { nameAr: 'أرض', nameEn: 'Land' },
-          { nameAr: 'مكتب', nameEn: 'Office' },
+          {
+            nameAr: 'تاون هاوس',
+            nameEn: 'Townhouse',
+            customFields: {
+              create: [
+                {
+                  key: 'countryId',
+                  type: 'array',
+                  labelAr: 'الدولة',
+                  labelEn: 'Country',
+                },
+                {
+                  key: 'cityId',
+                  type: 'array',
+                  labelAr: 'المدينة',
+                  labelEn: 'City',
+                },
+                {
+                  key: 'age',
+                  type: 'number',
+                  labelAr: 'العمر',
+                  labelEn: 'Age',
+                },
+                {
+                  key: 'numberOfRooms',
+                  type: 'number',
+                  labelAr: 'عدد الغرف',
+                  labelEn: 'Number Of Rooms',
+                },
+                {
+                  key: 'totalArea',
+                  type: 'number',
+                  labelAr: 'المساحة الكلية',
+                  labelEn: 'Total Area',
+                },
+                {
+                  key: 'numberOfFloors',
+                  type: 'number',
+                  labelAr: 'عدد الطوابق',
+                  labelEn: 'Number Of Floors',
+                },
+              ],
+            },
+          },
+          {
+            nameAr: 'وحدة',
+            nameEn: 'Unit',
+            customFields: {
+              create: [
+                {
+                  key: 'countryId',
+                  type: 'array',
+                  labelAr: 'الدولة',
+                  labelEn: 'Country',
+                },
+                {
+                  key: 'cityId',
+                  type: 'array',
+                  labelAr: 'المدينة',
+                  labelEn: 'City',
+                },
+                {
+                  key: 'age',
+                  type: 'number',
+                  labelAr: 'العمر',
+                  labelEn: 'Age',
+                },
+                {
+                  key: 'numberOfRooms',
+                  type: 'number',
+                  labelAr: 'عدد الغرف',
+                  labelEn: 'Number Of Rooms',
+                },
+                {
+                  key: 'totalArea',
+                  type: 'number',
+                  labelAr: 'المساحة الكلية',
+                  labelEn: 'Total Area',
+                },
+              ],
+            },
+          },
+          {
+            nameAr: 'فيلا',
+            nameEn: 'Villa',
+            customFields: {
+              create: [
+                {
+                  key: 'countryId',
+                  type: 'array',
+                  labelAr: 'الدولة',
+                  labelEn: 'Country',
+                },
+                {
+                  key: 'cityId',
+                  type: 'array',
+                  labelAr: 'المدينة',
+                  labelEn: 'City',
+                },
+                {
+                  key: 'age',
+                  type: 'number',
+                  labelAr: 'العمر',
+                  labelEn: 'Age',
+                },
+                {
+                  key: 'numberOfRooms',
+                  type: 'number',
+                  labelAr: 'عدد الغرف',
+                  labelEn: 'Number Of Rooms',
+                },
+                {
+                  key: 'totalArea',
+                  type: 'number',
+                  labelAr: 'المساحة الكلية',
+                  labelEn: 'Total Area',
+                },
+                {
+                  key: 'numberOfFloors',
+                  type: 'number',
+                  labelAr: 'عدد الطوابق',
+                  labelEn: 'Number Of Floors',
+                },
+              ],
+            },
+          },
+          {
+            nameAr: 'أرض',
+            nameEn: 'Land',
+            customFields: {
+              create: [
+                {
+                  key: 'countryId',
+                  type: 'array',
+                  labelAr: 'الدولة',
+                  labelEn: 'Country',
+                },
+                {
+                  key: 'cityId',
+                  type: 'array',
+                  labelAr: 'المدينة',
+                  labelEn: 'City',
+                },
+                {
+                  key: 'landType',
+                  type: 'array',
+                  labelAr: 'نوع الأرض',
+                  labelEn: 'Land Type',
+                },
+                {
+                  key: 'totalArea',
+                  type: 'number',
+                  labelAr: 'المساحة الكلية',
+                  labelEn: 'Total Area',
+                },
+              ],
+            },
+          },
+          {
+            nameAr: 'مكتب',
+            nameEn: 'Office',
+            customFields: {
+              create: [
+                {
+                  key: 'countryId',
+                  type: 'array',
+                  labelAr: 'الدولة',
+                  labelEn: 'Country',
+                },
+                {
+                  key: 'cityId',
+                  type: 'array',
+                  labelAr: 'المدينة',
+                  labelEn: 'City',
+                },
+                {
+                  key: 'age',
+                  type: 'number',
+                  labelAr: 'العمر',
+                  labelEn: 'Age',
+                },
+                {
+                  key: 'numberOfRooms',
+                  type: 'number',
+                  labelAr: 'عدد الغرف',
+                  labelEn: 'Number Of Rooms',
+                },
+                {
+                  key: 'totalArea',
+                  type: 'number',
+                  labelAr: 'المساحة الكلية',
+                  labelEn: 'Total Area',
+                },
+              ],
+            },
+          },
         ],
       },
     },
@@ -75,10 +454,10 @@ async function main() {
             labelEn: 'Color',
           },
           {
-            key: 'type',
+            key: 'carType',
             type: 'array',
-            labelAr: 'النوع',
-            labelEn: 'Type',
+            labelAr: 'نوع العربية',
+            labelEn: 'Car Type',
           },
         ],
       },
