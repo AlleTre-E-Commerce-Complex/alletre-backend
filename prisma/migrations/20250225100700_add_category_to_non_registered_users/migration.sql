ef662d3040d085c1422aa89d92b8db8d33e9e1c9
Alletre <alletre.auctions@gmail.com>
2025-02-25 12:23:18 +0400

    chore: remove redundant migration file

--- prisma/migrations/20250225100700_add_category_to_non_registered_users/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "NonRegisteredUser" ADD COLUMN "categoryId" INTEGER;
-
--- AddForeignKey
-ALTER TABLE "NonRegisteredUser" ADD CONSTRAINT "NonRegisteredUser_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category"("id") ON DELETE SET NULL ON UPDATE CASCADE;
