0f4bf02b271958ddac58c17a135eec5258fa0af9
Alletre <alletre.auctions@gmail.com>
2025-05-18 14:13:07 +0400

    fix: Completed-profit-setup

--- src/payments/services/payments.service.ts
+++ src/payments/services/payments.service.ts
@@ -4197,7 +4197,7 @@ export class PaymentsService {
     const newDate =
       process.env.NODE_ENV === 'production'
         ? new Date(date.getTime() + hours * 60 * 60 * 1000)
-        : new Date(date.getTime() +   15 * 60 * 1000); 
+        : new Date(date.getTime() +   10 * 60 * 1000); 
     // const newDate = new Date(date.getTime() + 6 * 60 * 1000);
 
     return newDate;
