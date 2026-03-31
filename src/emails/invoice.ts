1f75970751a8b665598f4442c6708f79deddce84
rameez1010 <muhammedrameez1010@gmail.com>
2026-03-31 16:31:55 +0400

    fix: Image removed

--- src/emails/invoice.ts
+++ src/emails/invoice.ts
@@ -22,9 +22,6 @@ export const generateInvoicePDF = async (invoiceData: any): Promise<Buffer> => {
   <div style="max-width: 750px; margin: 0 auto;">
     <div style="display: flex; justify-content: space-between; align-items: flex-start; margin-bottom: 30px;">
       <div style="display: flex; flex-direction: column;">
-        <img src="https://firebasestorage.googleapis.com/v0/b/alletre-auctions.firebasestorage.app/o/allatre-logo-color.svg?alt=media&token=d39db059-121a-4db5-9006-d609f3060b99"
-             alt="Alletre Logo"
-             style="height: 36px; margin-bottom: 10px; margin-right: 180px;">
         <div style="font-size: 16px; line-height: 1.8;">
           <strong>ALLE TRE E-Commerce Complex LLC OPC</strong><br>
           Office No. 504<br>
